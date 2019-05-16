eaab2569bd545f8ea25541b7779f771f3f250652
nohseungwon <hgpno21@gmail.com>
2019-05-16 20:24:06 +0900

    ignore set

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ module.exports = {
   dbConnection: mysql.createConnection({
     host: 'all-list.cvo8zxv2zp0l.ap-northeast-2.rds.amazonaws.com',
     user: 'root',
-    password: 'list9089',
+    password: 'blabla',
     database: 'all'
   })
 }
