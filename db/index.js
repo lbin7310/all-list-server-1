f3153f9ad82a94d33a921b061e05a4231ba432eb
nohseungwon <hgpno21@gmail.com>
2019-05-17 09:27:26 +0900

    really dot problem?

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ module.exports = {
   dbConnection: mysql.createConnection({
     host: 'all-list.cvo8zxv2zp0l.ap-northeast-2.rds.amazonaws.com',
     user: 'root',
-    password: 'wwwww',
+    password: 'wwwwssssw',
     database: 'all'
   })
 }
