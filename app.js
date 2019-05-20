a2c572a86212809d81e3f1be06ad49d947d4d4c6
nohseungwon <hgpno21@gmail.com>
2019-05-20 22:16:22 +0900

    middle commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const router = require('./routes');
 const cookieParser = require('cookie-parser');
 
 app.use(cors({})) // options 해결하기 위한 cors 미들웨어 사용
-app.use(cookieParser()); // 쿠키사용 router 보다 상위에 있어야함
+// app.use(cookieParser()); // 쿠키사용 router 보다 상위에 있어야함
 app.use(express.json())// body 바로 볼 수 있게 셋팅
 
 app.use('/', router); // 라우터로 보내기
--- models/board.js
+++ models/board.js
@@ -1,11 +1,11 @@
 const db = require('../db/index');
 
 module.exports = {
-  get: (boardIdx, callback) => {
-    console.log("board get ", boardIdx);
+  get: (origin_board_idx, callback) => {
+    console.log("board get ", origin_board_idx);
     let query = "SELECT * FROM `all`.Board inner join `all`.List on Board.origin_board_idx = List.board_idx " +
     "inner join `all`.Card on List.origin_list_idx = Card.list_idx where Board.origin_board_idx = ?"
-    db.dbConnection.query(query, [boardIdx], (err, data) => {
+    db.dbConnection.query(query, [origin_board_idx], (err, data) => {
       if (err) { return callback(err, null) }
       console.log("board data", data);
       return callback(null, data);
@@ -13,11 +13,11 @@ module.exports = {
   },
 
   create: (data, callback) => {
-    console.log("create data ", data);
-    let { title, idx, is_private, desc } = data;
+    console.log("board create data ", data);
+    let { board_title, origin_user_idx, is_private, desc } = data;
     // console.log("board create mysql", idx);
     let query = "INSERT INTO `all`.Board (`board_title`, `owner_idx`, `is_private`, `board_desc`) VALUES (?,?,?,?);"
-    db.dbConnection.query(query, [title, idx, is_private, desc], (err, data) => {
+    db.dbConnection.query(query, [board_title, origin_user_idx, is_private, desc], (err, data) => {
       if (err) {
         console.log(err);
         return callback(err, null)
